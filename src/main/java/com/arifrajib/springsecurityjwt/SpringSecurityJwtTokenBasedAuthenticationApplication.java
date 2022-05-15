179809cee41294bcb5472f25d71e6312f50ac005
Arif Islam <arif.rajib@gmail.com>
2022-05-15 11:29:25 -0700

    First commit from project initialization

---
+++ src/main/java/com/arifrajib/springsecurityjwt/SpringSecurityJwtTokenBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.arifrajib.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtTokenBasedAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtTokenBasedAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arifrajib/springsecurityjwt/SpringSecurityJwtTokenBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifrajib.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtTokenBasedAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
